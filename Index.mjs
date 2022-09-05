bae92f926b34d00d414d99bd0da18b681dfd6432
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-09-05 18:02:39 +0200

    finally node is working correctly

---
+++ Index.mjs
@@ -0,0 +1,8 @@
+import { appendFileSync } from 'node:fs';
+
+try {
+  appendFileSync('message.txt', 'data to append');
+  console.log('The "data to append" was appended to file!');
+} catch (err) {
+  /* Handle the error */
+}
